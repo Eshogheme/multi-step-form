c08eee6127fd5ff101c534cf022f52b93d0ac1fb
eshogheme <sabokingsley4@gmail.com>
2024-09-13 21:08:38 -1100

    chore: tried to fix the bug in the plansection

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavSection } from "./NavSection";
 import { InputSection } from "./InputSection";
 import { PlanSection } from "./PlanSection";
 import "./App.css";
 
 function App() {
-
-
   return (
     <>
       <Router>
-      
-      <NavSection />
-        <Routes>
-        <Route path="/" element={<InputSection />} /> 
-        <Route path="/step2" element={<PlanSection />} />
-        </Routes>
+       
+        <NavSection />
         
+        
+        <Routes>
+          <Route path="/" element={<InputSection />} /> 
+          <Route path="/step2" element={<PlanSection />} />
+        </Routes>
       </Router>
     </>
   );
